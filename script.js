f23b75cbaf26b2a92da82ef0c63da91f4a2c0206
Adam <adamgjohnson92@gmail.com>
2023-06-17 13:05:39 -0700

    Added Robots and Aliens to vibeArray.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const vibeButton = document.querySelector("#vibe-button")
 function generateVibe(){
     
     const generatedVibeDiv = document.querySelector("#generated-vibe")
-    const vibeArray = ["Black", "White", "Black and White", "Red", "Green", "Blue", "Yellow", "Orange", "Purple", "Pink", "Brown", "Grey", "SuperHeroes", "Star Wars", "Anime", "Fantasy", "High School Superlatives", "Animal Friends"]
+    const vibeArray = ["Black", "White", "Black and White", "Red", "Green", "Blue", "Yellow", "Orange", "Purple", "Pink", "Brown", "Grey", "Super Heroes", "Star Wars", "Anime", "Fantasy", "High School Superlatives", "Animal Friends", "Tough Guy", "Sports", "Food", "Samurai", "My Squad", "Robot", "Alien"]
 
     var randomVibe = vibeArray[Math.floor(Math.random() * vibeArray.length)];
     console.log(randomVibe)
